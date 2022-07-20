be49ffee549d857ad0254d503dbad97cd1fa1d57
redcat-24 <redcat_24@outlook.com>
2022-07-20 18:38:52 -0500

    Update 2
    -Added Poison Dart and Dart Textures
    -Added Aluminum Block (WIP)

--- src/main/java/com/redcat24/upsidedown/RedcatsUpsideDown.java
+++ src/main/java/com/redcat24/upsidedown/RedcatsUpsideDown.java
@@ -1,5 +1,7 @@
 package com.redcat24.upsidedown;
 
+import com.redcat24.upsidedown.block.ModBlocks;
+import com.redcat24.upsidedown.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
@@ -10,12 +12,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RedcatsUpsideDown implements ModInitializer {
+	public static final String MOD_ID = "upsidedown";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Logger LOGGER = LoggerFactory.getLogger("upsidedown");
-	public static final Item POISON_DART = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("upsidedown", "poison_dart"), POISON_DART);
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/com/redcat24/upsidedown/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.redcat24.upsidedown.item;
+
+import com.redcat24.upsidedown.RedcatsUpsideDown;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item DART = registerItem("dart", new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item POISON_DART = registerItem("poison_dart", new Item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(RedcatsUpsideDown.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        RedcatsUpsideDown.LOGGER.info("Registering modded items for " + RedcatsUpsideDown.MOD_ID);
+    }
+
+}
